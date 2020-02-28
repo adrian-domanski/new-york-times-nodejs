0ccc88299fbdf1eb834a0634a6ce904b21a8d4f8
Adrian <adrian.domanski.dev@gmail.com>
2020-02-28 08:41:35 +0100

    Export folder

--- server/server.js
+++ server/server.js
@@ -25,14 +25,14 @@ app.use(
 const PORT = process.env.PORT || 5000;
 
 // // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "out", "index.html"));
+  });
+}
 
 // mongoose.set("debug", true);
 mongoose.connect(
